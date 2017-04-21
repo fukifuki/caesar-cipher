6d9f0f47132b66c07b4ce3a72490df0170b2c903
Rade Krstanovic <radekrstanovic@gmail.com>
2017-04-21 16:19:45 +0200

    Add GEt route handler with a basic view.

--- main.rb
+++ main.rb
@@ -1,6 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require './lib/caesar_cipher'
+
 
 get '/' do
-  "Hello, world!"
+  @encoded_message = "biljance"
+  erb :home
 end
+
+
+__END__
+@@home
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Caesar Cipher</title>
+</head>
+<body>
+  <p><%= @encoded_message %><p>
+</body>
+</html>
