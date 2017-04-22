082af332ce19c40196313db6f9451833c868c48f
Rade Krstanovic <radekrstanovic@gmail.com>
2017-04-22 13:16:38 +0200

    Add submit form

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require './lib/caesar_cipher'
 
 
 get '/' do
-  @encoded_message = "biljance"
+  @encoded_message = ""
   erb :home
 end
 
@@ -17,6 +17,15 @@ __END__
   <title>Caesar Cipher</title>
 </head>
 <body>
+  <header>
+    <h1>Caesar Cipher</h1>
+  </header>
+  <form action '/' method='GET'>
+    <textarea name="message" rows="10" cols="50" placeholder="Enter your message here..." autofocus></textarea><br />
+    <p>Enter the key</p>
+    <input type="number" name="key" />
+    <input type="submit" value="Encode!" /> 
+  </form>
   <p><%= @encoded_message %><p>
 </body>
 </html>
